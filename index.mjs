64716d58955bc319eb03ea24f01ca14480fb0b4e
yash <yashchetan77@gmail.com>
2026-04-14 16:33:40 +0530

    Refactor database connection: move pg Pool configuration to a separate db.js file

--- index.mjs
+++ index.mjs
@@ -1,6 +1,5 @@
 import 'dotenv/con'
 import express from "express";
-import pg from "pg";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import cors from "cors";
@@ -9,20 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const port = process.env.PORT || 8080;
 
-// Equivalent to mongoose connection
-// Pool is nothing but group of connections
-// If you pick one connection out of the pool and release it
-// the pooler will keep that connection open for sometime to other clients to reuse
-const pool = new pg.Pool({
-  host: "localhost",
-  port: 5433,
-  user: "postgres",
-  password: "postgres",
-  database: "sql_class_2_db",
-  max: 20,
-  connectionTimeoutMillis: 0,
-  idleTimeoutMillis: 0,
-});
+
 
 const app = new express();
 app.use(cors());
---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+import pg from "pg";
+
+// Equivalent to mongoose connection
+// Pool is nothing but group of connections
+// If you pick one connection out of the pool and release it
+// the pooler will keep that connection open for sometime to other clients to reuse
+const pool = new pg.Pool({
+    host: "localhost",
+    port: 5433,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASS,
+    database: "postgres",
+    max: 20,
+    connectionTimeoutMillis: 0,
+    idleTimeoutMillis: 0,
+});
+
+
+export default pool;
